6a857fd29fb702a3e1760aaccfc76cc4d40c6c2d
Luis Eduardo Imbernón <imbernon@gmail.com>
2023-12-31 20:16:44 +0100

    Fixed some errors for the download from the S3 bucket

--- src/download.py
+++ src/download.py
@@ -5,12 +5,11 @@ import sys
 
 
 def download_file_from_s3(bucket_name, file_name, local_path, region, access_key, secret_key):
+    print(f"Access Key ID '{access_key}' AWS Secret '{secret_key}' FILE NAME '{file_name}' LOCAL PATH '{local_path}'.")
     s3 = boto3.client('s3', region_name=region, aws_access_key_id=access_key, aws_secret_access_key=secret_key)
-    boto3.client()
 
     try:
         s3.download_file(bucket_name, file_name, local_path)
-        print(f"File '{file_name}' downloaded successfully to '{local_path}'.")
     except NoCredentialsError:
         print("Error: AWS credentials not found. Make sure to configure them correctly.")
 
@@ -23,12 +22,12 @@ with open(params_file, 'r') as fd:
 
 # Getting all the download parameters
 bucket_n = params['download']['bucket_name']
-files_to_download = params['download']['bucket_name']
-local_destination_path = params['download']['bucket_name']
+files_to_download = params['download']['files_to_download']
+local_destination_path = params['download']['local_destination_path']
 reg = params['download']['bucket_region']
 ak = params['download']['access_key']
 sk = params['download']['secret_key']
 
 # Downloading all the files
 for file in files_to_download:
-    download_file_from_s3(bucket_n, file, local_destination_path + '/' + file, reg, ak, sk)
+    download_file_from_s3(bucket_n, file, str(local_destination_path) + '/' + str(file), reg, ak, sk)
